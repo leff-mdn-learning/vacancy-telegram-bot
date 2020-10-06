4c2f6ccbd976317b92e3fe840c4926260ebb3867
Alexandr Yakovlev <leffalt@gmail.com>
2020-10-06 14:40:08 +0700

    Created model Vacancies

---
+++ src/Model/Vacancy.php
@@ -0,0 +1,122 @@
+<?php
+
+
+namespace AYakovlev\Model;
+
+
+class Vacancy extends ActiveRecordEntity
+{
+    protected string $title;
+    protected int $price;
+    protected string $organization;
+    protected string $address;
+    protected string $telephone;
+    protected string $experience;
+    protected string $technology;
+    protected string $skills;
+    protected string $conditions;
+    protected string $datecreation;
+    protected int $reserv;
+    protected string $category;
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOrganization(): string
+    {
+        return $this->organization;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTelephone(): string
+    {
+        return $this->telephone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getExperience(): string
+    {
+        return $this->experience;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTechnology(): string
+    {
+        return $this->technology;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSkills(): string
+    {
+        return $this->skills;
+    }
+
+    /**
+     * @return string
+     */
+    public function getConditions(): string
+    {
+        return $this->conditions;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDatecreation(): string
+    {
+        return $this->datecreation;
+    }
+
+    /**
+     * @return int
+     */
+    public function getReserv(): int
+    {
+        return $this->reserv;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCategory(): string
+    {
+        return $this->category;
+    }
+
+    protected static function getTableName(): string
+    {
+        return 'vacancies';
+    }
+}
