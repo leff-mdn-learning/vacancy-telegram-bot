3edaafe87be8e70e454b3076015fc19798b3cbb9
Alexandr Yakovlev <leffalt@gmail.com>
2020-10-06 13:28:10 +0700

    Add token check to controller and header

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,16 +4,22 @@
 namespace AYakovlev\Controller;
 
 
+use AYakovlev\Core\UsersAuthService;
 use AYakovlev\Core\View;
 use AYakovlev\Model\Article;
+use AYakovlev\Model\User;
 
 class BlogController
 {
     private View $view;
+    /** @var User|null */
+    private ?User $user;
 
     public function __construct()
     {
+        $this->user = UsersAuthService::getUserByToken();
         $this->view = new View();
+        $this->view->setVar('user', $this->user);
     }
 
     public function index(): void
